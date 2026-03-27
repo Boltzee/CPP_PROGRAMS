fd90f8e26d258b5f03dca44c2870e757796943f0
Kiran Tirunagiri <tsaikiran506@gmail.com>
2026-03-28 04:11:52 +0530

    github codespaces support added

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  vim \
+  build-essential \
+  openssl \
+  ssh \
+  fish \
+  tmux \
+  xz-utils \
+  wget \
+  gcc \
+  g++ \
+  gdb \
+  make \
+  kitty-terminfo
+
+# Set locale
+locale-gen en_US.UTF-8
+update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+echo 'export LANG=en_US.UTF-8' >> ~/.bashrc
+
+mkdir -p ~/.local/bin  # ← add this before the tar command
+curl -LO https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz
+tar -xf zig-linux-x86_64-0.13.0.tar.xz -C ~/.local/
+ln -sf ~/.local/zig-linux-x86_64-0.13.0/zig ~/.local/bin/zig
