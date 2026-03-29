ac7c760fbb3d42a7a8491fbbe9cbdb1b64c3dbce
Kiran Tirunagiri <tsaikiran506@gmail.com>
2026-03-29 15:23:48 +0530

    update setup.sh to install eza & bat

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -24,7 +24,23 @@ apt-get install -y \
   stow \
   locales \
   software-properties-common \
+  gpg \
   kitty-terminfo
+
+# Install eza
+apt-get install -y gpg
+mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list
+chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+apt-get update
+apt-get install -y eza
+
+# Install bat
+BAT_VERSION=$(curl -s "https://api.github.com/repos/sharkdp/bat/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
+curl -LO "https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/bat_${BAT_VERSION}_amd64.deb"
+dpkg -i bat_${BAT_VERSION}_amd64.deb
+rm bat_${BAT_VERSION}_amd64.deb
 
 add-apt-repository -y ppa:zhangsongcui3371/fastfetch
 apt-get update
